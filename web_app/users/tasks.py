f64d9e412edc9ee9e57cf1295d03c3c7819380a0
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-06-29 14:29:52 +0300

    created recount balance of streamer task

---
+++ web_app/users/tasks.py
@@ -0,0 +1,14 @@
+from celery import shared_task
+from celery_singleton import Singleton
+from django.db.models import Sum
+from .models import StreamerModel, DonateModel
+
+
+@shared_task(base=Singleton)
+def recount_total_sum(donate_pk: DonateModel.pk):
+    streamer = StreamerModel.objects.get(user=DonateModel.objects.get(payment_id=donate_pk).streamer.user)
+    donates_related_to_streamer = DonateModel.objects.filter(
+        streamer=streamer).filter(withdrawn=False, payment__status='succeeded')
+    streamer.balance = donates_related_to_streamer.aggregate(sum=Sum('payment__payment_sum'))['sum'] or 0
+    streamer.count_donations = donates_related_to_streamer.count()
+    streamer.save()

897d51d41b336890e03699d9c3154b9875fc366a
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-06-29 15:03:15 +0300

    created log(in/out) and greet views

--- web_app/users/views.py
+++ web_app/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from social_django.utils import psa
 
-# Create your views here.
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    return render(request, 'users/html/login.html')
+
+
+def greet(request):
+    return render(request, 'users/html/greet_page.html')
+
+
+@psa('social:complete')
+def logout_social(request):
+    request.backend.logout(request.user)
+    return redirect('/')

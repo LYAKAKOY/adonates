786c003c1eb0762f8a34dd8bc50ef7a9d0313660
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-06-29 16:24:20 +0300

    created signals for withdraw

---
+++ web_app/payments/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from payments.models import PayoutModel
+from users.models import StreamerModel
+
+
+@receiver(post_save, sender=PayoutModel)
+def set_balance_zero(sender, instance: PayoutModel, created: bool, **kwargs) -> None:
+    if created and instance.status == 'succeeded':
+        streamer = StreamerModel.objects.get(user=instance.streamer)
+        streamer.balance = 0
+        streamer.save()

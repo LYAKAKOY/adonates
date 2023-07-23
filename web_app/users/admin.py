288c45d04d6f4b9c588bed17a42f1b08d8bbd97f
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-07-23 16:05:05 +0300

    settings admin panel

--- web_app/users/admin.py
+++ web_app/users/admin.py
@@ -6,8 +6,12 @@ class StreamerAdmin(admin.ModelAdmin):
     search_fields = ['user__username', ]
 
 
+class StreamerCardAdmin(admin.ModelAdmin):
+    search_fields = ['streamer__user__username', ]
+
+
 admin.site.register(StreamerModel, StreamerAdmin)
 admin.site.register(DonateModel)
-admin.site.register(StreamerCard)
+admin.site.register(StreamerCard, StreamerCardAdmin)
 admin.site.register(StreamerSettings)
 admin.site.register(StreamerGoal)

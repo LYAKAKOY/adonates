8f7679c6243f404d8c78a05198a3fcd6e7808df1
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-07-23 15:02:49 +0300

    debug=False start project

--- web_app/config/settings.py
+++ web_app/config/settings.py
@@ -22,9 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^k(1hm)@1_msrpri%(!klljty=&303s--#=pa@4umitfz0kwfg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost'
+]
 
 # Application definition
 
